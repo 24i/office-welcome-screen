58c1d92ca1c108ea50a5285d489bade3c55c1164
Bas Tuijnman <bas.tuijnman@24i.com>
2015-12-02 09:43:11 +0100

    fix padding, fix timing, fix month name

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,20 @@
 
     var clock = document.getElementById('clock'),
         clockInterval = null,
-        monthMap = {1:'Januari',2:'Februari',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'},
+        months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ],
         setClock,
         startClock;
 
@@ -11,22 +24,30 @@
      * Start the clock
      */
     setClock = function () {
-        var now = new Date();
-        clock.innerHTML =
-            now.getHours()
+        var now = new Date(),
+            pad = function (str) {
+                if (typeof str !== 'string') {
+                    str = '' + str;
+                }
+                return str.length === 1 ? '0' + str : str;
+            }
+        clock.innerHTML = ''
+            + pad(now.getHours())
             + ':'
-            + now.getMinutes()
+            + pad(now.getMinutes())
             + '<br />'
-            + monthMap[now.getMonth()]
+            + months[now.getMonth()]
             + ' '
-            + now.getDay();
+            + pad(now.getDay());
     }
 
     startClock = function () {
+        setClock();
         if (clockInterval === null) {
             setInterval(setClock, 60 * 1000);
         }
     };
+
     setClock();
     setTimeout(startClock, (60 - (new Date()).getSeconds()) * 1000);
 
